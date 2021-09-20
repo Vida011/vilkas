6397856f8854a2e609d14930f92fbc32c090fe40
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-09-20 11:00:02 +0300

    komponentai su sąlygom

--- src/Components/Bebras.jsx
+++ src/Components/Bebras.jsx
@@ -1,6 +1,6 @@
 // bus Komponenetas Komponenete ir susidės iš vieno span'o
 
-function Bebras(props) { Bebrui reikia propso Bebras
+function Bebras(props) { // Bebrui reikia propso Bebras
     return (
             <span>{props.bebras}</span> // šitie propsai nueis į App.jsx komponentą ir bus atvaizduoti
         );
--- src/Components/Kvadratas.jsx
+++ src/Components/Kvadratas.jsx
@@ -2,14 +2,14 @@ import Bebras from './Bebras';
 
 function Kvadratas(props) {
     if ('Brigadininkas' == props.bebras) {
-        return (
-            <div className={Kvadratas}>
-            <b><Bebras bebras={props.bebras}/></b> //čia turim Bebro komponentą
+        return (                // jeigu grąžina šitą, tai
+            <div className='Kvadratas br'>
+                <b><Bebras bebras={props.bebras}/></b>     //čia turim Bebro komponentą
             </div>
             );
     }
     
-return (
+return (                        // šito jau nebegrąžins
     <div className={Kvadratas}>
         <Bebras bebras={props.bebras}/> //čia turim Bebro komponentą
     </div>
