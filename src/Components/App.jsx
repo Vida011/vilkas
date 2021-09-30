e50f44fcba5d31b8869df1e4d1f81992727660bd
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-09-30 15:39:26 +0300

    Todo pradžia

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,14 +1,23 @@
 import React, {useState, useEffect, useLayoutEffect} from 'react';
+import axios from 'axios';
 
 function App() {
 
+    const [todos, setTodos] = useState;
+
+
     useEffect (()=> {
         console.log('start');
+        axios.get('https://jsonplaceholder.typicode.com/todos/1')
+        .then(function (response) {
+            console.log(response.data);
+        })
     
     }, []);
 
-    return (<>
-       Labas
+    return (<div className="todo-container">
+        {todos.map((todo)=>(<Todo key = {todo.id} data={todo}></Todo>))}
+     
         </>);
     }
 
---
+++ src/Components/todo.jsx
@@ -0,0 +1,13 @@
+
+
+
+function Todo (props) {
+
+    return (
+        <div></div>
+
+    )
+
+}
+
+export default Todo;
