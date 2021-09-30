69f683a761ca3732e0688eecef98bc8cea63abd2
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-09-30 16:49:02 +0300

    user

--- src/Components/todo.jsx
+++ src/Components/todo.jsx
@@ -1,10 +1,18 @@
-
+import React, {useState, useEffect} from "react";
+import axios from "axios";
+import Todo from './Todo';
 
 
 function Todo (props) {
 
     return (
-        <div></div>
+        <div className="todo">
+            <div className="id">{props.data.id}</div>
+            <div className="title">{props.data.title}</div>
+            <div className="status" style={
+                {backgroundColor: props.data.completed === false? 'red' : 'green'}
+                }></div>
+        </div>
 
     )
 
