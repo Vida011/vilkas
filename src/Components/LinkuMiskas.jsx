8dbfe7e73b84194a3921e2d911ff8c90069f43d0
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-10-03 23:13:02 +0300

    Street komponentas

--- src/Components/LinkuMiskas.jsx
+++ src/Components/LinkuMiskas.jsx
@@ -1,17 +1,19 @@
 import { Link, Switch, Route } from "react-router-dom";
+import Street from './Street';
+
 function LinkuMiskas() {
   return (
     <div className="forest">
       <nav>
-        <Link to={"home"}>
-          <h3>Linku Miskas</h3>
-        </Link>
-        <Link to={"road"}>
-          <h3>Misko plentas</h3>
-        </Link>
-        <Link to={"street"}>
-          <h3>Misko gatve</h3>
-        </Link>
+        <Link to={"home"}><h3>Linku Miskas</h3></Link>
+        <Link to={"road"}><h3>Misko plentas</h3></Link>
+        <div>
+        <Link to={"/1"}><h3>Misko gatve 1</h3></Link>
+        <Link to={"/2"}><h3>Misko gatve 2</h3></Link>
+        <Link to={"/3"}><h3>Misko gatve 3</h3></Link>
+        <Link to={"/4"}><h3>Misko gatve 4</h3></Link>
+        <Link to={"/5"}><h3>Misko gatve 5</h3></Link>
+        </div>
       </nav>
       <Switch>
         <Route path={"/home"}>
@@ -20,9 +22,10 @@ function LinkuMiskas() {
         <Route path={"/road"}>
           <h1>The Road</h1>
         </Route>
-        <Route path={"/street"}>
-          <h1>The Street</h1>
-        </Route>
+        
+        <Route path={"/:street"} children={<Street/>} />
+  
+     
         <Route path={"/"}>
           <h1>The Begining</h1> //čia komponentas renderinasi būtent tas, kurio mums reikia
         </Route>
---
+++ src/Components/Street.jsx
@@ -0,0 +1,8 @@
+
+function Street () {
+    return(
+        <h1>Street</h1>
+    )
+}
+
+export default Street;
