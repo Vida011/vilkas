699f314168bc3a1aee909ac9f1ff5ed181415681
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-10-03 17:10:38 +0300

    Knygos

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,6 +1,7 @@
 import React, {useState, useEffect,} from 'react';
 import axios from 'axios';
 import Knyga from './Knyga';
+import Sort from './Sort';
 
 function App() {
 
@@ -11,14 +12,39 @@ function App() {
         axios.get('https://in3.dev/knygos/')
         .then(function (response) {
             console.log(response.data);
-            setBooks(response.data);
+            const book = response.data;
+            books.sort((a, b)=> {
+                if (a.title > b.title) {
+                    return 1;
+                }
+                else if(a.title < b.title) {
+                    return -1;
+                }
+                else {
+                    return 0;
+                }
+
+            })
+            setBooks(books);
         })
     
     }, []);
 
-    return (<div className="books-container">
-        {books.map((book)=>(<Knyga key = {book.id} data={book}></Knyga>))}
-     
+    const makeSort = (dir) => {
+        console.log(dir); // pasitikrinam ar tikrai gaunam
+        const booksCopy = books.slice();
+        if(dir === 'priceDesc') {
+            booksCopy.sort((a, b)) => a.price-b.price);
+            setBooks(bookCopy)
+
+    }
+}
+    return (
+        <div>
+        <Sort makeSort={makeSort}></Sort>
+        <div className="books-container">
+        {books.map((book)=>(<Knyga key ={book.id} data={book}></Knyga>))}
+        </div>
         </div>);
     }
 
