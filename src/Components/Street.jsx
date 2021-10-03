cceefa9a3c5d3fa97417afcf069d861b0481340d
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-10-03 23:54:48 +0300

    useParams

--- src/Components/Street.jsx
+++ src/Components/Street.jsx
@@ -1,7 +1,11 @@
+import { useParams } from 'react-router-dom';
 
 function Street () {
+
+    const { street } = useParams();// kintamasis street turi sutapti su kint.pagal dvitaski(:street)
+
     return(
-        <h1>Street</h1>
+        <h1>Street {street}</h1>
     )
 }
 
