a07cba0b88783bdc22a537eeef7f12c157733ad3
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-10-03 22:03:53 +0300

    Router1

---
+++ src/Components/LinkuMiskas.jsx
@@ -0,0 +1,34 @@
+import { Link, Switch, Route } from "react-router-dom";
+function LinkuMiskas() {
+  return (
+    <div className="forest">
+      <nav>
+        <Link to={"home"}>
+          <h3>Linku Miskas</h3>
+        </Link>
+        <Link to={"road"}>
+          <h3>Misko plentas</h3>
+        </Link>
+        <Link to={"street"}>
+          <h3>Misko gatve</h3>
+        </Link>
+      </nav>
+      <Switch>
+        <Route path={"/home"}>
+          <h1>The Home</h1>
+        </Route>
+        <Route path={"/road"}>
+          <h1>The Road</h1>
+        </Route>
+        <Route path={"/street"}>
+          <h1>The Street</h1>
+        </Route>
+        <Route path={"/"}>
+          <h1>The Begining</h1> //čia komponentas renderinasi būtent tas, kurio mums reikia
+        </Route>
+      </Switch>
+    </div>
+  );
+}
+
+export default LinkuMiskas;
