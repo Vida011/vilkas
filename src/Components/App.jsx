2a139dc0fd1160177c92667b183328f3e279947e
Vida011 <84325506+Vida011@users.noreply.github.com>
2021-10-05 22:36:08 +0300

    useRef

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,18 +1,40 @@
-import { useRef } from "react";
+import { useRef, useState, useEffect } from "react";
 
 function App() {
-    const counter = useRef(0);
-    const [turboCounter, setTurboCounter] = useState(0);
+  const counter = useRef(0);
+  const crazyRef = useRef();
+  const [turboCounter, setTurboCounter] = useState(0);
 
-    const clicker = () = {
-        counter.current++;
-        console.log('kaunteris:', counter.current);
-    }
-    return(<>
-    <div>REF: {counter.current}</div
+  const clicker = () => {
+    counter.current++;
+  };
 
-    </div>
+  const turboClicker = () => {
+    setTurboCounter(turboCounter + 1);
+  };
 
-    
-    )
+  useEffect(() => {
+    console.log(crazyRef.current);
+    crazyRef.current.style.fontSize = "50px";
+    //
+    document.querySelector(".crazy").style.color = "blue";
+
+    setTimeout(() => {
+      document.querySelector(".crazy").innerHTML = "JA JA!";
+    }, 2000);
+  }, []);
+
+  return (
+    <>
+      <h1>REF: {counter.current}</h1>
+      <h1>STATE: {turboCounter}</h1>
+      <button onClick={clicker}>REF: Ja Ja!</button>
+      <button onClick={turboClicker}>STATE: Ja Ja!</button>
+      <div ref={crazyRef} className="crazy">
+        CRAZY
+      </div>
+    </>
+  );
 }
+
+export default App;
